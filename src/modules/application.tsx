9ccc05dea6197e7da9ecb69658c27dff27e9cf5f
Vegard Falldalen <vegfall@gmail.com>
2024-03-06 14:10:39 +0100

    Added ol to our solution

---
+++ src/modules/application.tsx
@@ -0,0 +1,30 @@
+import React, { MutableRefObject, useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import "ol/ol.css";
+import { OSM } from "ol/source";
+import TileLayer from "ol/layer/Tile";
+import { useGeographic } from "ol/proj";
+
+useGeographic();
+
+const map = new Map({
+  view: new View({
+    center: [11, 60],
+    zoom: 10,
+  }),
+  layers: [new TileLayer({ source: new OSM() })],
+});
+
+export default function Application() {
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  }, []);
+
+  return (
+    <>
+      <main ref={mapRef}></main>
+    </>
+  );
+}
